237ed7ee0e4a08f2648ee1dca3f284c762f09176
AJAY YADAV <58135870+ajay767@users.noreply.github.com>
2020-04-19 19:55:28 +0530

    uploade the file

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector(".nav-menu").addEventListener('click', () =>{
+  
+document.querySelector(".links").classList.toggle('style-nav');
+
+
+document.querySelector(".bar1").classList.toggle('bar-style');
+document.querySelector(".bar2").classList.toggle('bar-style');
+document.querySelector(".bar3").classList.toggle('bar-style');
+
+
+document.querySelector(".nav-menu").classList.toggle('style-menu');
+
+});
